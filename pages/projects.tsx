dcd25f642cf9d99ed1b3d31a72c75a8a60083cd4
brett <brettszlachciuk@gmail.com>
2023-03-10 15:34:44 -0500

    made project cards aligned correctly

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,25 +2,25 @@ import { ProjectCard } from "../components/ProjectCard";
 export default function Projects() {
   return (
     <div className="bg-gradient-to-b from-gray-200 to-gray-100">
-      <ul className="flex flex-col items-center w-full space-y-16">
-        <li className="mr-auto ml-auto">
+      <ul className="flex flex-col justify-items-center space-y-16">
+        <li className="w-full mx-72">
           <ProjectCard
             projectTitle="Politician Database Application"
             technologies="Java, SQL"
             imgSrc="politician"
           ></ProjectCard>
         </li>
-        <li className="mr-auto ml-auto">
+        <li className="w-full mx-72">
           <ProjectCard
             projectTitle="Strava Extension"
             technologies="JavaScript, React, Express, REST"
             imgSrc="strava"
           ></ProjectCard>
         </li>
-        <li className="mr-auto ml-auto">
+        <li className="w-full mx-72">
           <ProjectCard
             projectTitle="Machine Learning Stock Price Prediction"
-            technologies="Python, PyTorch, PyChart, APIs"
+            technologies="Python, Pandas, Transformers, Data Visualization, APIs"
             imgSrc="ml"
           ></ProjectCard>
         </li>
