ce2ed8075c41f4f266e44a864b18543f98800abf
Brett Szlachciuk <brettszlachciuk@gmail.com>
2023-03-13 18:39:41 -0400

    added resume modal with iframe

--- components/resumeModal.tsx
+++ components/resumeModal.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import SinglePage from "./resume";
-import resume from "../public/Resume_Brett.pdf";
 
 export default function ResumeModal() {
   const [showResume, setShowResume] = useState(false);
@@ -14,14 +12,14 @@ export default function ResumeModal() {
         Resume
       </button>
       {showResume ? (
-        <div className="fixed inset-0 bg-opacity-25 backdrop-blur flex justify-center items-center">
+        <div className="fixed inset-0 bg-opacity-25 backdrop-blur flex flex-col justify-center items-center">
           <button
             className=" bg-gradient-to-r from-pink-500 to-purple-200 px-4 text-gray-900 py-2 rounded-md ml-8 mr-4 text-lg font-bold"
             onClick={() => setShowResume(false)}
           >
             Close
           </button>
-          <SinglePage props={resume} />
+          <iframe src={"../Resume_Brett.pdf"} width={900} height={900} />
         </div>
       ) : null}
     </>
